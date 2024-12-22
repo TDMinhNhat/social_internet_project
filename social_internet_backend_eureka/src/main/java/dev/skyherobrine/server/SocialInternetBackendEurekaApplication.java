f4af0384e2ae5872ea7484aef02c45d2f940bc77
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-22 19:28:07 +0700

    Eureka: configure server

--- social_internet_backend_eureka/src/main/java/dev/skyherobrine/server/SocialInternetBackendEurekaApplication.java
+++ social_internet_backend_eureka/src/main/java/dev/skyherobrine/server/SocialInternetBackendEurekaApplication.java
@@ -2,8 +2,10 @@ package dev.skyherobrine.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SocialInternetBackendEurekaApplication {
 
     public static void main(String[] args) {
