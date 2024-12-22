07972ec46930aed62a50287887f5be971b1b931d
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-22 19:25:22 +0700

    Eureka: init project

---
+++ social_internet_backend_eureka/src/main/java/dev/skyherobrine/server/SocialInternetBackendEurekaApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialInternetBackendEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialInternetBackendEurekaApplication.class, args);
+    }
+
+}
---
+++ social_internet_backend_eureka/src/test/java/dev/skyherobrine/server/SocialInternetBackendEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialInternetBackendEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
