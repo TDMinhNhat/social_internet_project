5ec53effdec576800ccd64143752ee280f26c87b
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-12-22 23:03:34 +0700

    Fanpage: init project

---
+++ social_internet_backend_eureka/fanpage_service/src/main/java/dev/skyherobrine/service/FanpageServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.skyherobrine.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanpageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanpageServiceApplication.class, args);
+    }
+
+}
